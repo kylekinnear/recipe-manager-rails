b0338a5c1e7d29d075193012e90d721babe6d0eb
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 08:53:51 -0400

    logged_in?, require_logged_in, current_user methods added to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :current_user
   before_action :require_logged_in, except: [:new, :create, :home]
 
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def require_logged_in
+    redirect_to root-path unless logged_in?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
