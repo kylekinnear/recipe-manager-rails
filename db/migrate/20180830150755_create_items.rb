41fa3a2a7b9a7ce291f3a19ea2aaa935ea74e62a
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 11:16:14 -0400

    fixed createitems migration

--- db/migrate/20180830150755_create_items.rb
+++ db/migrate/20180830150755_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.integer :quantity
       t.string :unit
-      t.name :ingredient
+      t.string :ingredient
 
       t.timestamps
     end
