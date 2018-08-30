5a76607286cf139f287e8eeab0cdacb3b2bbf3b3
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 08:46:59 -0400

    user new and user create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,18 @@ class UsersController < ApplicationController
     @message ||= false
   end
 
+  def new
+    @user  = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      #redirect
+    else
+      #redirect
+    end
+  end
+
 end
