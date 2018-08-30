d6937fab3f46733388eaa3de481cc3dfbac81e66
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 11:49:50 -0400

    recipes controller before actions, index, show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @message = params[:message] if params[:message]
+    @message ||= false
+  end
+  
 end
