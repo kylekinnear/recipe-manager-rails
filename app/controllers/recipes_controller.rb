a7bd47b4a5bc452f49213bde4b8d90b7f422c77a
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 12:09:58 -0400

    recipes controller create notes, edit, update methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,5 +9,23 @@ class RecipesController < ApplicationController
     @message = params[:message] if params[:message]
     @message ||= false
   end
-  
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    #we need to create the recipe by converting the textarea param into a set of items, which we create and use to associate or create ingredients
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe), notice: 'Recipe was successfully updated.'
+    else
+      render :edit
+    end
+
 end
