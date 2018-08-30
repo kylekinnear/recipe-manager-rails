db8bdb7879d25dcd94c2e92b493f7743e339508f
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 08:49:25 -0400

    set_user and user_params methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,14 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
 end
