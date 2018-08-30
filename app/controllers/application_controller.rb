937a8154b32600d7b65bed312a0c9f4b7e426b7e
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 17:32:51 -0400

    fixed typo in application controller require_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def require_logged_in
-    redirect_to root-path unless logged_in?
+    redirect_to root_path unless logged_in?
   end
 
   def current_user
