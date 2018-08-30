ee1d829d8243060bbec293d825ca4428e8c6d75b
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 10:58:57 -0400

    sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :recipes
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+  
 end
