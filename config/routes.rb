b9373867a04d38da458d495c5f738ecff50366d2
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 23:15:32 -0400

    nested recipe routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
   root 'static#index'
-  
+
+  resources :users do
+    resources :recipes, only: [:show, :index]
+  end
+
   resources :ingredients
   resources :recipes
-  resources :users
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
 
 end
