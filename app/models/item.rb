d9f3ff0635ef397fb4cebf6357b9f22eeef022dc
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-30 21:49:39 -0400

    added serves to recipes, testing item/ingredient associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
+  attr_accessor :ingredient
   has_and_belongs_to_many :ingredients
   belongs_to :recipe
 
   def output
-    self.quantity.to_s + " " + self.unit + " " + self.ingredient.name
+    self.quantity.to_s + " " + self.unit + " " + self.ingredient_name
   end
 
 end
