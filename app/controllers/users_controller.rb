44f419fd152559135d168a3a8d71820af07039c4
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 07:50:14 -0400

    updated user new view and users controller for email field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :email)
   end
 
 end
