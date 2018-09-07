18e9af7aacd6f2777ea466d4aea6854dbb208433
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 11:07:26 -0400

    user method find_or_create_by_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,13 @@ class User < ApplicationRecord
   has_many :recipes
   #has_many :ingredients, :through => :recipes #does this work, since we're skipping items?
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+      user.name = auth_hash["info"]["name"]
+      user.password = SecureRandom.hex
+    end
+  end
+
   def highest_rated
     ranker = {}
     self.recipes.each do |r|
