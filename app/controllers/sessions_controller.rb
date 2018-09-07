45cf3ae2a9852c876bbc517542fd614e8370a0e4
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 11:11:19 -0400

    omniauth login in sessionscontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if auth_hash = request.env["omniauth.auth"]
       @user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to root_path
     else
       @user = User.find_by(name: params[:user][:name])
       if @user && @user.authenticate(params[:user][:password])
