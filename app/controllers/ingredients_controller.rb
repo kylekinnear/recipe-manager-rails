d80c59c0b635d543d1ad1d067e8db7a50690be3d
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 04:43:22 -0400

    ingredient#show and ingredient#most_used controller actions built

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,17 @@
 class IngredientsController < ApplicationController
 
   def show
+    @ingredient = Ingredient.find(params[:id])
+    @message = params[:message] if params[:message]
+    @message ||= false
   end
 
   def index
   end
 
   def most_used
+    @ingredient = Ingredient.most_used
+    redirect_to ingredient_path(@ingredient)
   end
-  
+
 end
