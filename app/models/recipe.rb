0a1dfbce5c01018c95bb60cb3008945c6389222f
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 07:55:28 -0400

    fixed recipe has_an_ingredient? validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,10 +11,10 @@ class Recipe < ApplicationRecord
   has_many :ingredients, :through => :items
 
   def has_an_ingredient?
-    if self.items.first.name.nil?
+    if self.items.first.ingredient_name.nil?
       errors.add(:items, "You need at least 1 ingredient")
     else
-      unless !!(self.ingredient_name =~ /[a-zA-Z]/)
+      unless !!(self.items.first.ingredient_name =~ /[a-zA-Z]/)
         errors.add(:items, "Your ingredient needs some letters")
       end
     end
