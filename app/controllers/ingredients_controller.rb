ddc29fd51370b784d2456915286663998984f3ab
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 04:39:40 -0400

    ingredient routes and controller methods

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,12 @@
 class IngredientsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def most_used
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
     resources :recipes, only: [:show, :index, :new]
   end
 
-  resources :ingredients
+  resources :ingredients, only: [:show, :index]
   resources :recipes
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
+  get 'ingredients/most_used', to: 'ingredients#most_used'
 
 
 end
