b4b59b254195c9412824eeddfbf1f7508cadad9e
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 07:38:53 -0400

    added a custom validation to recipe for having an ingredient

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
 
   validates :name, presence: true
+  validate :has_an_ingredient?
 
   belongs_to :user
 
@@ -8,5 +9,15 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :items
 
   has_many :ingredients, :through => :items
+
+  def has_an_ingredient?
+    if self.items.first.name.nil?
+      errors.add(:items, "You need at least 1 ingredient")
+    else
+      unless !!(self.ingredient_name =~ /[a-zA-Z]/)
+        errors.add(:items, "Your ingredient needs some letters")
+      end
+    end
+  end
 
 end
