2ace441e482b80f5eb08f7a7ea858db2951ff093
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 07:19:46 -0400

    added emails to users

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_08_30_150755) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
