5aeea24a5b9e59ce9b1b82496598e2d060dffa89
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-07 04:47:23 -0400

    ingredient#index controller action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,6 +7,12 @@ class IngredientsController < ApplicationController
   end
 
   def index
+#    if params[:user_id]
+#      @user = User.find(params[:user_id])
+#      @ingredients = @user.ingredients
+#    else
+      @ingredients = Ingredient.all
+#    end
   end
 
   def most_used
