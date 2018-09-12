21d5d86b625c49be042102fbdb5c3986b6040036
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-12 11:05:20 -0400

    removed emails from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :password, :email)
+    params.require(:user).permit(:name, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ require 'securerandom'
 
 class User < ApplicationRecord
 
-  #validates :email, presence: true
-  #validates :email, uniqueness: true
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :password, presence: true
