bfbb7ae3641b49433e32ccc05a1c832441c2d001
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-09-12 14:03:57 -0400

    added dependent: :destroy to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
 
   belongs_to :user
 
-  has_many :items
+  has_many :items, dependent: :destroy
   accepts_nested_attributes_for :items
 
   has_many :ingredients, :through => :items
